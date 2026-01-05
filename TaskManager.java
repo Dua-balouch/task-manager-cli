b42361b588ee4a42a9513dce000678954ba66ec0
Dua Balouch <dua.67718@iqra.edu.pk>
2026-01-06 01:55:50 +0500

    Add delete-task by Dua

--- TaskManager.java
+++ TaskManager.java
@@ -1,15 +1,21 @@
 import java.util.ArrayList;
 
 public class TaskManager {
+
     static ArrayList<String> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
+
         addTask("Learn Git Desktop");
+        addTask("Learn Java");
+        showTasks();
+
+        deleteTask("Learn Java");
         showTasks();
     }
 
     public static void addTask(String task) {
-        if(task == null || task.isEmpty()) {
+        if (task == null || task.isEmpty()) {
             System.out.println("Task cannot be empty");
         } else {
             tasks.add(task);
@@ -19,14 +25,17 @@ public class TaskManager {
 
     public static void showTasks() {
         System.out.println("All Tasks:");
-        for(String task: tasks) {
+        for (String task : tasks) {
             System.out.println("- " + task);
         }
     }
-}
-public static void main(String[] args) {
-    addTask("Learn Git Desktop");
-    addTask("Learn Java Branching");
-    showTasks();
-}
 
+    public static void deleteTask(String task) {
+        if (tasks.contains(task)) {
+            tasks.remove(task);
+            System.out.println("Task deleted successfully: " + task);
+        } else {
+            System.out.println("Task not found: " + task);
+        }
+    }
+}
