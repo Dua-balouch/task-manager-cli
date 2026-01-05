a9a838ca06a64a5b736995a34e204e9d51731a6e
Dua Balouch <dua.67718@iqra.edu.pk>
2026-01-06 01:07:08 +0500

    Create TaskManager.java

---
+++ TaskManager.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class TaskManager {
+
+    static ArrayList<String> tasks = new ArrayList<>();
+
+    public static void main(String[] args) {
+        // Initial test
+        addTask("Learn Git Desktop");
+        showTasks();
+    }
+
+    public static void addTask(String task) {
+        if(task == null || task.isEmpty()) {
+            System.out.println("Task cannot be empty");
+        } else {
+            tasks.add(task);
+            System.out.println("Task added successfully: " + task);
+        }
+    }
+
+    public static void showTasks() {
+        System.out.println("All Tasks:");
+        for (String task : tasks) {
+            System.out.println("- " + task);
+        }
+    }
+}
